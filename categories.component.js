13e518b9ff2ff1d67b261bfd116df48955640eef
punetideepika <punetideepika@gmail.com>
2024-05-11 17:58:11 +0530

    Add files via upload

---
+++ categories.component.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+  
+    angular.module('MenuApp')
+      .component('categories', {
+        templateUrl: 'categories.template.html',
+        bindings: {
+          categories: '<'
+        }
+      });
+  
+  })();
+  
